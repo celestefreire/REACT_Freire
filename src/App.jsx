c30df4774b028b9b2221304f33c19870071bc962
Celeste Freire <cian.celestefreire@gmail.com>
2021-11-30 21:20:10 -0300

    navbar ok

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style/style.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <div className='fondo'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
